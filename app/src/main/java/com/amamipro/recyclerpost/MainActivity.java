f134d9364ca7e6e7463cd8a55f29deb9f4400736
f3rry12 <f3rry12@gmail.com>
2019-03-29 13:45:31 +0700

    Initial commit

---
+++ app/src/main/java/com/amamipro/recyclerpost/MainActivity.java
@@ -0,0 +1,108 @@
+package com.amamipro.recyclerpost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.CacheControl;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static String urlpost = "http://jsonplaceholder.typicode.com/posts";
+    private List<PostModel> listPost = new ArrayList<PostModel>();
+
+    private RecyclerView lView;
+    PostAdapter adapter;
+    private String responseBody;
+    private JSONObject obj;
+    private OkHttpClient client = new OkHttpClient();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.);
+
+        ambildata();
+        lView = (RecyclerView) findViewById(R.id.recycler_post);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        lView.setLayoutManager(layoutManager);
+        adapter = new PostAdapter(this,listPost);
+        lView.setAdapter(adapter);
+
+    }
+
+    private void ambildata(){
+//        Request request = new Request().Builder()
+//                .url(urlpost)
+//                .cacheControl(new CacheControl.Builder().noCache().build())
+//                .build();
+        Request request = new Request.Builder()
+                .url(urlpost)
+                .cacheControl(new CacheControl.Builder().noCache().build())
+                .build();
+
+        client.newCall(request).enqueue((new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("ON FAILURE",e.getStackTrace().toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(!response.isSuccessful()){
+                    throw new IOException("(gak respon jsonnya)Unexpected Code " + response);
+                } else {
+                    final String hasil = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            parse(hasil);
+                        }
+                    });
+                }
+            }
+        }));
+    }
+
+    public void parse(String strjson){
+        try {
+            Log.d("STRJSON",strjson);
+            JSONArray jsonarray = new JSONArray(strjson);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                PostModel post = new PostModel();
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                String userId = jsonobject.getString("userID");
+                String id = jsonobject.getString("id");
+                String title = jsonobject.getString("title");
+                String body = jsonobject.getString("body");
+                post.setUserId(userId);
+                post.setId(id);
+                post.setTitle(title);
+                post.setBody(body);
+
+            }
+            adapter.notifyDataSetChanged();
+        } catch (Throwable t) {
+            Log.e("My App", "Jseon orr: \"" + strjson + "\"");
+        }
+        }
+
+}
+
---
+++ app/src/main/java/com/amamipro/recyclerpost/PostAdapter.java
@@ -0,0 +1,58 @@
+package com.amamipro.recyclerpost;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+    private List<PostModel> listPost;
+    private Activity activity;
+
+    public PostAdapter(Activity activity, List<PostModel> listPost) {
+        this.listPost = listPost;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listpost,viewGroup,false);
+        ViewHolder vh = new ViewHolder(v);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.judul.setText(listPost.get(i).getTitle());
+        viewHolder.isi.setText(listPost.get(i).getBody());
+        //button
+    }
+
+    @Override
+    public int getItemCount() {
+        return listPost.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView judul;
+        private TextView isi;
+        private Button tombolkomen;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            judul = itemView.findViewById(R.id.post_judul);
+            isi = itemView.findViewById(R.id.post_body);
+            tombolkomen = itemView.findViewById(R.id.see_comment);
+        }
+    }
+
+
+}
